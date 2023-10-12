b1aad56162f049d91d87a350549f53bec9c51a8b
Mareks1993 <marekstaszkiewicz@live.com>
2023-10-11 22:15:49 -0400

    updated app.js as well as created new.js and new.pug to handle /new route for posts

--- routes/index.js
+++ routes/index.js
@@ -1,20 +1,12 @@
 var express = require('express');
 var router = express.Router();
-const messages = [
-  {
-    text: "Hi there!",
-    user: "Amando",
-    added: new Date()
-  },
-  {
-    text: "Hello World!",
-    user: "Charles",
-    added: new Date()
-  }
-];
+
+
+var messages = require('./messagesModule');
 /* GET home page. */
 router.get('/', function(req, res, next) {
   res.render('index', { title: 'Message Board', messages: messages });
 });
+
 
 module.exports = router;
---
+++ routes/new.js
@@ -0,0 +1,33 @@
+var express = require('express');
+var router = express.Router();
+// GET /new to display the form
+
+
+var messages = require('./messagesModule');
+
+
+router.use(express.urlencoded({ extended: true }));
+
+
+router.get('/', function(req, res, next) {
+    res.render('new', { title: 'New Message' });
+  });
+  
+  // POST /new to handle form submission
+  router.post('/', function(req, res, next) {
+    // Get the message from the request body (you'd typically use middleware like body-parser for this)
+    const text = req.body.text;
+    
+    // Add the new message to the messages array
+    messages.unshift({
+      text: text,
+      user: "Anonymous", // Or fetch the username from session or another source
+      added: new Date()
+    });
+    
+    // Redirect back to the main page
+    res.redirect('/');
+  });
+  
+
+  module.exports = router;
