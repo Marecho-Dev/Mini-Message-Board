bc14c8acfbc31f3f41fc3db53c6a4a9909f245d0
Mareks1993 <marekstaszkiewicz@live.com>
2023-10-12 01:35:08 -0400

    updated index.js to be async and pull all messages from database and pass that into the view

--- routes/index.js
+++ routes/index.js
@@ -1,11 +1,16 @@
 var express = require('express');
 var router = express.Router();
-
+const Message = require('../models/message');
 
 var messages = require('./messagesModule');
 /* GET home page. */
-router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Mini Messageboard', messages: messages });
+router.get('/', async function(req, res, next) {
+  try {
+    const messages = await Message.find().sort({ added: -1 }); // Fetch all messages, sorted by date in descending order.
+    res.render('index', { title: 'Mini Messageboard', messages });
+  } catch (err) {
+    next(err); // Handle the error, e.g., by passing it to an error-handling middleware.
+  }
 });
 
 
